d39ef392b02a9aafd631784c408d9b271a422505
Rodevp <rodriguezrodrigor12@gmail.com>
2022-09-04 19:56:06 -0500

    metodos

--- express.js
+++ express.js
@@ -11,6 +11,30 @@ app.get('/', (req, res) => {
     res.send('Hello world') //send da más información en la respuesta
 })
 
+//metodos http
+
+app.get('/user', (req, res) => {
+    res.send('usuarios')
+})
+
+app.post('/user', (req, res) => {
+    res.send('creando usuario')
+})
+
+app.put('/user', (req, res) => {
+    res.send('actualizando usuario') 
+})
+
+app.patch('/user', (req, res) => {
+    res.send('actualizando una parte del usuario') 
+})
+
+app.delete('/user', (req, res) => {
+    res.send('eliminando usuario')
+})
+
+
+
 app.use((req, res) => { //en caso de no encontrar las rutas va tomar esta.
     res.status(404).send('not found')
 })
